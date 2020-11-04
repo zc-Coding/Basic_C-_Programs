7c4c88a5ba0e4f7f5271e7be6ed9fe5c7541cb33
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-04 11:32:01 -0700

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Acounting Systems\nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
+
