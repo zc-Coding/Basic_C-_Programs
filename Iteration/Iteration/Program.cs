d2b4deae510efcd46ae05d8f90ecaec34dc129ba
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-11 15:11:13 -0700

    cleaned up repository,
    Moved BooleanComparisons pg 190 C# to the main directory, and got rid of the redundancies on IncomeComparison and VariablesAndDataTypes. there was a folder inside of a folder both with the same name in the file paths.

---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Zach", "Beans", "Means", "Mae" };
+
+            for (int j = 0; j < names.Length; j++)
+                if (names[j] == "Means")
+                {
+                    Console.WriteLine(j);
+                }
+
+
+
+            //int[] testScores = { 98, 94, 78, 87, 97, 89, 48, 87, 65 };
+
+            //for (int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+        }
+    }
+}
---
+++ VariableAndDataTypes/VariableAndDataTypes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VariableAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            // Intro Lines 
+            //            Console.WriteLine("The Tech Academy.\nStudent Daily Report.");
+
+            //            // 1st Question
+            //            Console.WriteLine("What is your name?");
+            //            string yourName = Console.ReadLine();
+
+            //            // 2nd Question 
+            //            Console.WriteLine("What course are you on?");
+            //            string yourCourse = Console.ReadLine();
+
+            //            // 3rd Question
+            //            Console.WriteLine("What page number?");
+            //            string yourPage = Console.ReadLine();
+
+            //            // True or False
+            //            Console.WriteLine("Do you need help with anything? Please answer true or false");
+            //            string needHelp = Console.ReadLine();
+
+            //            // 5th Question
+            //            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics");
+            //            string positiveExp = Console.ReadLine();
+
+            //            // 6th Question
+            //            Console.WriteLine("Is there any feed back you'd like to share? Please be specific");
+            //            string feedback = Console.ReadLine();
+
+            //            // 7th Question
+            //            Console.WriteLine("How many hours did you study today?");
+            //            string hrsStudied = Console.ReadLine();
+
+            //            // Thanks you line
+            //            Console.WriteLine("Thank you for your andwers. An Instuctor will resopnd to this shortly. Have a great day!");
+            //            Console.ReadLine();
+
+
+            string X = "Doug Doug ";
+            string Y = "walty palty.";
+            string R = X + Y;
+            Console.WriteLine(R);
+        }
+    }
+}
