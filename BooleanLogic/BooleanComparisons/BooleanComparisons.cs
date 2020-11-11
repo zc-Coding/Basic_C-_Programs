096db57e537f237bc52764e034c034cfbf7d4f52
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-11 10:21:54 -0700

    C# pg 190

---
+++ BooleanLogic/BooleanComparisons/BooleanComparisons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 5)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Going up");
+                i++;
+            }
+            while (i < 10);
+          
+        }
+    }
+}
