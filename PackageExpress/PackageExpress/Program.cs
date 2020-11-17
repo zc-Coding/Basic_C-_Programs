dfbcedbf38af528ce1d0e626a604d1066671f693
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-17 15:29:32 -0700

    re pushed pg 185

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -1,26 +1,35 @@
 ﻿using System;
-using System.Net;
 
-namespace ConsoleApp1
+namespace PackageExpress
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express.\nPlease follow the instructions below.\n\n" +
-                "Please enter the package weight:");
+               "Please enter the package weight:");
             int weight = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the package width:");
             int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter package height");
+            int height = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the package length:");
             int length = Convert.ToInt32(Console.ReadLine());
-            decimal quote = Convert.ToDecimal((((weight * width * length) * weight) / 100));
-            Console.WriteLine("Your estimated total for shipping this package is: $" + quote + ".00\nThank you!");
-            
-            
-            
-            
-            
+            int tooBig = weight + width + length;
+            if (tooBig > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express");
+            }
+            else
+            {
+                decimal quote = Convert.ToDecimal((((height * width * length) * weight) / 100));
+                Console.WriteLine("Your estimated total for shipping this package is: $" + quote + ".00\nThank you!");
+            }
+            Console.ReadLine();
+
+
+
+
             //int roomTemperature = 70;
 
             //Console.WriteLine("Hi, what is your name?");
