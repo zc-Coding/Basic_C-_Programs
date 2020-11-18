a2a77d92ce4c678b70f5b012f18bab0230949629
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-17 21:09:25 -0700

    Pg 226 C# push

---
+++ MethodOverload/MethodOverload/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverload
+{
+    class Class1
+    {
+        public static int Operation1(int i)
+        {
+            int numb = i;
+            return numb * 7 + 21;
+        }
+
+        public static double Operation1(double j)
+        {
+            int numb = Convert.ToInt32(j);
+            return numb + 2 / 3;
+        }
+
+        public static int Operation1(string k)
+        {
+            int numb = Convert.ToInt32(k);
+            return (numb * 7) + (7 / 7);
+
+
+            // Attempted return of an error message if the string couldn't be converted to a integer
+            //try
+            //{
+            //    int numb = Convert.ToInt32(k);
+            //    return (numb * 7) + (7 / 7);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return ex.meessage;
+            //}
+
+
+        }
+    }
+}
---
+++ MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Class1.Operation1(7));
+
+            Console.WriteLine(Class1.Operation1(7.34));
+
+            Console.WriteLine(Class1.Operation1("7"));
+        }
+    }
+}
