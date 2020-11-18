579b4a7a8be81fe3aaa89f282685a61235c275a5
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-17 17:50:37 -0700

    pg 223 C# push

---
+++ BasicMethodsApp/BasicMethodsApp/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMethodsApp
+{
+    class MathOperations
+    {
+        public static int Addition(int i)
+        {
+            int input = i;
+            return input + 25;
+        }
+
+        public static int Multiply(int i)
+        {
+            int input = i;
+            return input * 5;
+        }
+
+        public static int DivideByFive(int i)
+        {
+            int input = i;
+            return input / 5;
+        }
+    }
+}
---
+++ BasicMethodsApp/BasicMethodsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicMethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n" +
+                "Please enter a number, we will be doing a few math operations to it");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MathOperations.Addition(numb));
+            Console.WriteLine(MathOperations.Multiply(numb));
+            Console.WriteLine(MathOperations.DivideByFive(numb));
+        }
+    }
+}
