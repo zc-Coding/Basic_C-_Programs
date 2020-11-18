3b451fae83563302c5ddd3f6f98a27644b31a52e
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-18 13:04:32 -0700

    pg 228 C# push

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethod
+{
+    class Class1
+    {
+        public void Operations(int a, int b)
+        {
+            //equation for first input
+            int answer = a * 8 + 4;
+            //display for second input
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 twoInput = new Class1();
+            twoInput.Operations(3, 8);
+
+            int x = 7;
+            int y = 2;
+            twoInput.Operations(x, y);
+        }
+    }
+}
