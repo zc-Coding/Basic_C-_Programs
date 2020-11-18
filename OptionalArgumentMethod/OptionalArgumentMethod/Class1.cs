e1cfb5e1f09cb7cd12a14f9320b5f33d0220f4df
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-18 11:48:43 -0700

    pg 227 C# push

---
+++ OptionalArgumentMethod/OptionalArgumentMethod/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalArgumentMethod
+{
+    class Class1
+    {
+        public int OptionalArgs(int a, int b = 2)
+        {
+            return a * b;
+        }
+    }
+}
