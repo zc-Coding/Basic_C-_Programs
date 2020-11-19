a04bbbcbddb390b4cdf9826e1783554a291182d9
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-19 14:03:45 -0700

    pg 228 C# repush1

--- VoidMethod/VoidMethod/Program.cs
+++ VoidMethod/VoidMethod/Program.cs
@@ -6,13 +6,12 @@ namespace VoidMethod
     {
         static void Main(string[] args)
         {
-
+            //instantiation 
             Class1 twoInput = new Class1();
+            //passing in two numbers
             twoInput.Operations(3, 8);
-
-            int x = 7;
-            int y = 2;
-            twoInput.Operations(x, y);
+            //specifying parameters by name
+            twoInput.Operations(a:6, b:27);
         }
     }
 }
