2898b52cde286a234b53280dde614c85a17052cc
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-20 12:20:25 -0700

    pg 252 C# Push

---
+++ OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverloading
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FName { get; set; }
+
+        
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> Employees = new Employee();
+            Employees.Add(1, "Josh");
+
+        }
+    }
+}
