790e7608f3c7e15543c0aeee0d619fa873b7530c
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-23 12:14:57 -0700

    pg 259 C# push

---
+++ StuctAssignment/StuctAssignment/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StuctAssignment
+{
+    public struct Number
+    {
+        public decimal Amount;
+    }
+}
---
+++ StuctAssignment/StuctAssignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StuctAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number i;
+            i.Amount = 3.63354m;
+            Console.WriteLine(i);
+        }
+    }
+}
