9ce51b1df5f6fb27a32e41e9d06f64c6e5e7a170
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-23 11:07:04 -0700

    pg 255 C# push

---
+++ GenericsAssignment/GenericsAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+        
+    }
+}
---
+++ GenericsAssignment/GenericsAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<int> Numbers = new Employee<int>();
+            Numbers.Things = new List<int>() { 3, 5, 7 };
+            Numbers.Things.Add(3);
+            
+        }
+    }
+}
