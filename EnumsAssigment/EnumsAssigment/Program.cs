f7c51adbfd827c2a260818bf47ba59b745a7253c
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-23 11:04:20 -0700

    PG 261 C# push

---
+++ EnumsAssigment/EnumsAssigment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace EnumsAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week");
+            string dayOfWeek = Console.ReadLine();                 
+            
+            try
+            {
+                Weekday answer = (Weekday)Enum.Parse(typeof(Weekday), dayOfWeek, true);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            
+        }
+    }
+}
---
+++ EnumsAssigment/EnumsAssigment/Weekdays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsAssigment
+{
+    class Weekdays
+    {
+        public Weekday Weekday { get; set; }
+    }
+
+    public enum Weekday
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
+
