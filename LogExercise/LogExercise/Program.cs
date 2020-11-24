1fce1824836564a65a18f300a004763f4b4e11f8
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-24 16:41:17 -0700

    pg 297 C# push

---
+++ LogExercise/LogExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace LogExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YOU KNOW WHAT THIS IS GIVE ME A NUMBER NOOOWWWWWW...");
+            string numb = Convert.ToString(Console.ReadLine());
+            using StreamWriter file = new StreamWriter(@"C:\Users\zachc\Documents\TechAcademy\C# .NET\Basic_C#_Programs\LogExercise\Log.txt", true);
+            file.WriteLine(numb);
+            Console.WriteLine(numb + " THAT'S ALL YOU GOT?!?!");
+        }
+    }
+}
