74844357e3876e14932a304c39830bf6fcd5fe3b
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-25 13:12:07 -0700

    pg 310 C# push

---
+++ ChainConstructors/ChainConstructors/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainConstructors
+{
+    class Person
+    {
+        // Second constructor calling the first
+        public Person(string FName) : this(FName, "none", 1001)
+        {
+        }
+        //First and larger constructor
+        public Person(string firstN, string lastN, int ID)
+        {
+            FName = firstN;
+            LName = lastN;
+            MyID = ID;
+        }
+        // properties for Person class
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int MyID { get; set; }
+    }
+}
---
+++ ChainConstructors/ChainConstructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChainConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constant variable
+            const string constSTR = "The string that is a constant";
+            // the errorless use of var method
+            string FixesError()
+            {
+                var person1 = "This could actually be any data type... HOW FUN!";
+                return "This also fixes a minor error\n" + person1+ "\n" + constSTR;
+            }
+            Console.WriteLine(FixesError());
+        }
+    }
+}
