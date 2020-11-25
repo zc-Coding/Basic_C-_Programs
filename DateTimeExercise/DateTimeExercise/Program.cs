5e934f6ff585928bd3b93cfba31e2ef5a6ee8661
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-25 09:46:55 -0700

    pg 300 C# push

---
+++ DateTimeExercise/DateTimeExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDT = DateTime.Now;
+            Console.WriteLine("Hello the current time is.. " + currentDT);
+            Console.WriteLine("\nHow many hours would you like to go into the future?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            DateTime newDT = currentDT.AddHours(hours);
+            Console.WriteLine("Your destination is... " + newDT);
+            
+        }
+    }
+}
