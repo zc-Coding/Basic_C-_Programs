32ce4f6fd3b58769941e387d9e2328f91d4f4ecb
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-11-25 15:26:36 -0700

    pg 314 C# push

---
+++ ExceptionsExercise/ExceptionsExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            Console.WriteLine("How old are you?");
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+                if (age <= 0)
+                {
+                    Console.WriteLine("Your age is a whole positive non-zero number");
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You really messed up this time");
+                return;
+            }
+            DateTime today = DateTime.Now;
+            int year = today.Year;
+            int birthYear = year - age;
+            
+            Console.WriteLine(birthYear);
+
+        }
+    }
+}
