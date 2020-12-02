f0fe142849c59733d8c813e40150140f1399d1bd
zc-Coding <69733110+zc-Coding@users.noreply.github.com>
2020-12-02 15:50:24 -0700

    pg 451 C# push

---
+++ CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new StudentContext())
+            {
+
+                var stud = new Student() { FirstName = "Tony", LastName = "Bologna", HomePhone = "555-654-9845" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstDemo/CodeFirstDemo/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo
+{
+    class StudentContext : DbContext
+    {
+        public StudentContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
